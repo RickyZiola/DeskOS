287bb61930377804b66c9ed3516fcae69a937a73
RickyZiola <78122421+RickyZiola@users.noreply.github.com>
2021-12-09 11:43:33 -0500

    Update update_manager.py

--- update_manager.py
+++ update_manager.py
@@ -2,7 +2,6 @@ import filecmp
 import os
 import json
 import tkinter as tk
-import time
 root=tk.Tk()
 root.eval('tk::PlaceWindow . center')
 second_win = tk.Toplevel(root)
@@ -10,7 +9,12 @@ root.eval(f'tk::PlaceWindow {str(second_win)} center')
 updateData=tk.Label(root, text="Checking for updates...")
 updateData.pack()
 os.system("mkdir updatecmp; cd updatecmp; git clone https://github.com/RickyZiola/DeskOS.git")
-time.sleep(1)
+main=open("updatecmp/DeskOS/src/DeskOS/main.py")
+package=open("updatecmp/DeskOS/src/DeskOS/package.json")
+wmain=open("updatecmp/DeskOS/main.py", "w")
+wpackage=open("updatecmp/DeskOS/package.json", "w")
+wmain.write(main.read())
+os.system('rm -r -rf updatecmp/DeskOS/src; rmdir /S /Q updatecmp/DeskOS/src')
 try:
   package = json.load(open("src/DeskOS/package.json"))
 except:
@@ -19,30 +23,29 @@ if(filecmp.cmp("src/DeskOS/package.json", "updatecmp/DeskOS/package.json",shallo
   updateData.config(text="Update available! Do you want to update now?")
   updateData.update_idletasks()
   def update():
-    confirmButton.pack_forget()
-    cancelButton.pack_forget()
     os.system("cd src; rmdir /S /Q DeskOS; rm -r -rf DeskOS; git clone https://github.com/RickyZiola/DeskOS.git")
+    main=open("src/DeskOS/src/DeskOS/main.py")
+    package=open("src/DeskOS/src/DeskOS/package.json")
+    wmain=open("src/DeskOS/main.py", "w")
+    wpackage=open("src/DeskOS/package.json", "w")
+    wmain.write(main.read())
+    wpackage.write(package.read())
+    os.system("rm -r -rf src/DeskOS/src; rmdir /S /Q src/DeskOS/src; rm src/DeskOS/update_manager.py; del src/DeskOS/update_manager.py")
+
     try:
       package = json.load(open("src/DeskOS/package.json"))
     except:
       package = ""
-    updateData.config(text="Updated to DeskOS v" + package["version"])
+    #updateData.config(text="Updated to DeskOS v" + package["version"])
     updateData.update_idletasks()
-    time.sleep(5)
-    root.destroy()
   def cancel():
-    confirmButton.pack_forget()
-    cancelButton.pack_forget()
     updateData.config(text="Update cancelled.")
     updateData.update_idletasks()
-    time.sleep(5)
-    root.destroy()
   confirmButton=tk.Button(root, text="Yes", command=update)
   cancelButton=tk.Button(root, text="No", command=cancel)
   confirmButton.pack()
   cancelButton.pack()
 else:
-  updateData.config(text="Up to date, version: v" + package["version"])
+  #updateData.config(text="Up to date, version: v" + package["version"])
   updateData.update_idletasks()
-root.mainloop()
 os.system("rm -r -rf updatecmp; python src/DeskOS/main.py")
